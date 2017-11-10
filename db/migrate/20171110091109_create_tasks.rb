ece5d41f7a5d5c572a0957cd39069e68639c59e3
Tiffa <mslatiffab@gmail.com>
2017-11-10 20:14:16 +1100

    Generate the task

---
+++ db/migrate/20171110091109_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :description
+      t.datetime :due
+      t.string :context
+      t.integer :priority
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
