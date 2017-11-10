1a28c130d2b252f8c2664088470ac2af61bb9418
Tiffa <mslatiffab@gmail.com>
2017-11-10 20:34:34 +1100

    Generate tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,16 @@
+class TasksController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'tasks/new'
+
+  get 'tasks/edit'
+
+  get 'tasks/show'
+
+  get 'tasks/index'
+
+  get 'tasks/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
